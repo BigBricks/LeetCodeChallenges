7a15026a63da1ecc990d9721f7dd9669b02c0fe1
BigBricks <BSA6.23.94@gmail.com>
2018-11-07 16:20:16 -0500

    added 217

---
+++ 217. Contains Duplicate.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function(nums) {
+  if (nums.length < 2) return false;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
