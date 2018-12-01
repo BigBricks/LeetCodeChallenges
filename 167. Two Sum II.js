4286bc11ef7174a9bacc4a6d7ebed0737b4335df
BigBricks <BSA6.23.94@gmail.com>
2018-11-30 19:24:40 -0500

    167

---
+++ 167. Two Sum II.js
@@ -0,0 +1,10 @@
+var twoSum = function(numbers, target) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i + 1, j + 1];
+      }
+    }
+  }
+};
+// INDEX STARTS AT 1 BUT NOT HERE APPARENTLY
