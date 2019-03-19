ce7b98ce7a43e29576ba63a90ab0b6af0d107350
BigBricks <36277515+BigBricks@users.noreply.github.com>
2019-03-18 23:35:58 -0400

    Update 1. Two Sum.js

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -1,7 +1,9 @@
 var twoSum = function(nums, target) {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let q = i + 1; q < nums.length; q++) {
-      if (nums[i] + nums[q] === target) return [i, q];
+      if (nums[i] + nums[q] === target) {
+        return [i, q];
+      }
     }
   }
 };
